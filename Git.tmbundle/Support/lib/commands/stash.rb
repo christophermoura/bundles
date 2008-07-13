ad437fcd109dbf406a3ab6b5ca4ad2f85083af4e
Tapajós <tapajos@gmail.com>
2008-07-13 11:27:24 -0300

    Add mirror 'Git.tmbundle/'.

---
+++ Git.tmbundle/Support/lib/commands/stash.rb
@@ -0,0 +1,35 @@
+require ENV['TM_SUPPORT_PATH'] + '/lib/ui.rb'
+
+class SCM::Git::Stash < SCM::Git::CommandProxyBase
+  def list
+    base.command("stash", "list").split("\n").map do |line|
+      /^(.+?):(.+)$/.match(line)
+      name = $1
+      description = $2
+      /([0-9]+)/.match(name)
+      {:id => $1.to_i, :name => name, :description => description}
+    end
+  end
+  
+  def save(desciption = "")
+    params = []
+    params << desciption unless desciption.nil? || desciption.empty?
+    base.command("stash", "save", *params)
+  end
+  
+  def diff(name)
+    base.parse_diff(base.command("stash", "show", "-p", name))
+  end
+  
+  def apply(name)
+    base.command("stash", "apply", "--index", name)
+  end
+  
+  def pop(name)
+    base.command("stash", "pop", "--index", name)
+  end
+  
+  def clear
+    base.command("stash", "clear")
+  end
+end
